fd62fa57a00c15a6a258991b34748c7d3b08cc98
deep <deeppatel234@gmail.com>
2018-01-26 10:42:09 +0530

    ADD Schema Test

--- test/schema_test/schema_test.js
+++ test/schema_test/schema_test.js
@@ -2,3 +2,10 @@ var Fields = require('../../mongoorm').Fields
 
 var assert = require('chai').assert
 
+describe('Schema', function () {
+    describe('basictest', function () {
+        it('should return type integer', function () {
+            assert.deepEqual("abcde","abcd");
+        });
+    })
+})
