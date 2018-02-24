59fd9ec1e3922c6687be6e5c81d81a8d3627a428
Deep Patel <deeppatel234@gmail.com>
2018-02-24 17:15:33 +0530

    Improve Code

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-var express = require('express');
-var app = express();
-var logger = require('./logger');
+var express = require('express')
+var app = express()
+var logger = require('./logger')
 var mongoorm = require('./mongoorm')
 // MONGO ORM
 var dbConfig = {
-    dburl: "mongodb://localhost:27017",
-    dbname: "test"
+  dburl: 'mongodb://localhost:27017',
+  dbname: 'test'
 }
 
 mongoorm.setLogger({
-    info: logger.info,
-    error: logger.error  
-});
+  info: logger.info,
+  error: logger.error
+})
 
 // Modules
-var demo = require('./modules/demo');
-
 app.get('/', function (req, res) {
-    demo.abc();
-    res.send('Hello World');
-});
+  res.send('Hello World')
+})
 
 mongoorm.db.connect(dbConfig, function (err) {
-    if (err) {
-        console.error('Error in DB Connection : ', err);
-    }
-    app.listen(3000, function () {
-        logger.info("Server Started at localhost:3000");
-    });    
-});
+  if (err) {
+    console.error('Error in DB Connection : ', err)
+  }
+  app.listen(3000, function () {
+    logger.info('Server Started at localhost:3000')
+  })
+})
